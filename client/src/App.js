c6593b1a913a01a57fa1adb716974460b4e5b200
Michael Kroon <kroonology@gmail.com>
2019-09-17 16:15:25 -0600

    add Home, About, ItemView, NoMatch to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import NoMatch from './components/NoMatch';
+import {Container} from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div>
-      Howdy
-    </div>
-  );
-}
+const App = () => (
+  <React.Fragment>
+    <Container>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route component={NoMatch} />
+      </Switch>
+    </Container>
+  </React.Fragment>
+);
 
 export default App;
---
+++ client/src/components/ItemView.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Header} from 'semantic-ui-react';
+
+const ItemView = () => (
+  <Header as='h1'>ItemView</Header>
+);
+
+export default ItemView;
