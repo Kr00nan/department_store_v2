2803ea97302674251220955cedd0f2aa0b8454cd
Michael Kroon <kroonology@gmail.com>
2019-09-17 16:29:07 -0600

    create Departments

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Departments from './components/Departments';
 import NavBar from './components/NavBar';
 import NoMatch from './components/NoMatch';
 import {Container} from 'semantic-ui-react';
@@ -13,6 +14,7 @@ const App = () => (
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/about' component={About} />
+        <Route exact path='/departments' component={Departments} />
         <Route component={NoMatch} />
       </Switch>
     </Container>
---
+++ client/src/components/Departments.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Departments extends React.Component {
+  render() {
+    return (
+      <div>Hello!</div>
+    );
+  };
+};
+
+export default Departments;
