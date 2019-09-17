0167ee37df25286640e4df73e3a13dd2356903d4
Michael Kroon <kroonology@gmail.com>
2019-09-17 16:24:32 -0600

    create NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import NavBar from './components/NavBar';
 import NoMatch from './components/NoMatch';
 import {Container} from 'semantic-ui-react';
 
 const App = () => (
   <React.Fragment>
+    <NavBar />
     <Container>
       <Switch>
         <Route exact path='/' component={Home} />
