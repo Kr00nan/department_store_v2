4b8b7f73ea890e0457eb84b9dc8010c6e389efa9
Michael Kroon <kroonology@gmail.com>
2019-09-18 09:09:00 -0600

    build DepartmentsForm and DepartmentsView

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import {Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Departments from './components/Departments';
+import DepartmentsForm from './components/DepartmentsForm';
+import DepartmentView from './components/DepartmentsView';
 import NavBar from './components/NavBar';
 import NoMatch from './components/NoMatch';
 import {Container} from 'semantic-ui-react';
@@ -15,6 +17,8 @@ const App = () => (
         <Route exact path='/' component={Home} />
         <Route exact path='/about' component={About} />
         <Route exact path='/departments' component={Departments} />
+        <Route exact path='/departments/new' component={DepartmentsForm} />
+        <Route exact path='/departments/:department_id/items' component={DepartmentView} />
         <Route component={NoMatch} />
       </Switch>
     </Container>
--- client/src/components/Departments.js
+++ client/src/components/Departments.js
@@ -24,7 +24,7 @@ class Departments extends React.Component {
     if (departments.length <= 0)
       return <h2>No Departments</h2>
     return departments.map( department => (
-      <Card as={Link} to={`/api/departments/${department.id}/items`}>
+      <Card as={Link} to={`/api/departments/${department.id}/items`} key={department.id}>
         <Card.Content>
           <Card.Header>{department.name}</Card.Header>
         </Card.Content>
