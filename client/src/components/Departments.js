d6fcb05a688f465bb3fce73471fff3ddb3037b8a
Michael Kroon <kroonology@gmail.com>
2019-09-17 22:17:50 -0600

    build out Departments.js

--- client/src/components/Departments.js
+++ client/src/components/Departments.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import axios from 'axios';
+import {Card, Header} from 'semantic-ui-react';
 
 class Departments extends React.Component {
+  state = {departments: []};
+
+  componentDidMount() {
+    // make a get request with axios
+    axios.get('/api/departments')
+    .then( res => {
+      this.setState({departments: res.data});
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    // update state
+  }
+
+  renderDepartments = () => {
+    const {departments} = this.state;
+
+    if (departments.length <= 0)
+      return <h2>No Departments</h2>
+    return departments.map( department => (
+      <Card>
+        <Card.Content>
+          <Card.Header>{department.name}</Card.Header>
+        </Card.Content>
+      </Card>
+    ))
+  }
+
   render() {
     return (
-      <div>Hello!</div>
+      <div>
+        <Header as='h1'>Departments</Header>
+        <br />
+        <Card.Group>
+          {this.renderDepartments()}
+        </Card.Group>
+      </div>
     );
   };
 };
