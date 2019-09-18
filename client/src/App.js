407ff101597aaf82a5b7e71b479e42074ce4bd45
Michael Kroon <kroonology@gmail.com>
2019-09-18 09:23:28 -0600

    made some chances; no dice

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Departments from './components/Departments';
 import DepartmentsForm from './components/DepartmentsForm';
-import DepartmentView from './components/DepartmentsView';
+import DepartmentsView from './components/DepartmentsView';
 import NavBar from './components/NavBar';
 import NoMatch from './components/NoMatch';
 import {Container} from 'semantic-ui-react';
@@ -18,7 +18,7 @@ const App = () => (
         <Route exact path='/about' component={About} />
         <Route exact path='/departments' component={Departments} />
         <Route exact path='/departments/new' component={DepartmentsForm} />
-        <Route exact path='/departments/:department_id/items' component={DepartmentView} />
+        <Route exact path='/departments/:department_id/items' component={DepartmentsView} />
         <Route component={NoMatch} />
       </Switch>
     </Container>
